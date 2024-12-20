a35e64e59601c0e0301af56bbeb7dc696866304e
dvvrahmavvt <dewirahmawati9862@gmail.com>
2024-12-20 18:52:32 +0700

    Delete HouseRepository.java

--- HouseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.predict.houseprices.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.predict.houseprices.model.House;
-
-@Repository
-public interface HouseRepository extends JpaRepository<House, Long> {}
